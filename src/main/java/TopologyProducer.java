cecae8bd1dad5161848ee836a9860e981aaccc8c
unknown <I309939@SINN50873620A.apj.global.corp.sap>
2015-12-31 17:46:08 +0800

    no message

--- src/main/java/TopologyProducer.java
+++ src/main/java/TopologyProducer.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algorithms.Digraph;
 import edu.princeton.cs.algorithms.DigraphGenerator;
+import prefuse.data.Graph;
 
 /**
  * Created by I309939 on 12/31/2015.
@@ -10,5 +11,13 @@ public class TopologyProducer {
 
         Digraph dag = DigraphGenerator.dag(10, 10);
         System.out.println(dag);
+
+
+        Graph g =new Graph();
+        for(int i=0;i<10;i++){
+            
+        }
+
+
     }
 }
